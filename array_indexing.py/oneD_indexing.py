b9bb42e9c1933b213b7e4f05b2ee7a845c35f725
anuragkumar07070 <134268853+anuragkumar07070@users.noreply.github.com>
2024-06-20 14:58:14 +0530

    py

---
+++ array_indexing.py/oneD_indexing.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+one_dim = np.array([1,2,3,4,5])
+print(one_dim)
+
+# index 1
+print(one_dim[1])
+
+# index 2
+print(one_dim[2])
+
+#index 4
+print(one_dim[4])
+
+# print last index of an array
+print(one_dim[-1])
+
+# print second last index of an array
+print(one_dim[-2]) # 3 and 5
+
+
+print(one_dim[-1]+ one_dim[2])
